d429085cdaf83547049b7aecdb6ed119db10f097
tafiya <tafiapinkey@gmail.com>
2023-10-20 10:50:40 +0600

    fetch data from server

--- index.js
+++ index.js
@@ -79,6 +79,13 @@ async function run() {
       res.send(result);
 
   })
+   //get the user
+   app.get('/users',async(req,res)=>{
+    const cursor =userCollection.find();
+    const result =await cursor.toArray();
+    res.send(result);
+})
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
