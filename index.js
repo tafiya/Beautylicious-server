917e5633acd0f433695e46bf4c2f8d445dbeb54c
tafiya <tafiapinkey@gmail.com>
2023-10-20 11:02:40 +0600

    delete from server is added

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
     const result =await cursor.toArray();
     res.send(result);
 })
+   //delete a user
+   app.delete('/users/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query={_id: new ObjectId(id)}
+    const result= await userCollection.deleteOne(query);
+    res.send(result)
+})
 
 
     // Send a ping to confirm a successful connection
